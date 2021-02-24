25e130fe9fe19650654f3a317de0047bdaa0beee
孟斌 <mengb15@chinaunicom.cn>
2021-02-24 15:15:13 +0800

    Initialize Project

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mastermeng/goweb
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/yuin/goldmark v1.3.2
+)
---
+++ handler.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
+)
+
+func hello(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"Content": "hello world"})
+}
+
+func content(ctx *gin.Context) {
+	var buf bytes.Buffer
+
+	input, err := ioutil.ReadFile(filepath.Clean("./articles/playground.md"))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	md := goldmark.New(goldmark.WithExtensions(extension.GFM),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithUnsafe(),
+			html.WithHardWraps(),
+		),
+	)
+
+	if err = md.Convert(input, &buf); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.HTML(http.StatusOK, "index.html", gin.H{"Content": template.HTML(buf.String())})
+}
